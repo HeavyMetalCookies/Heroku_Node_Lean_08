2a1eac098449535750837e681738d092f6d3f663
JOHN_MARK_MADISON <HeavyMetalCookies@GMail.com>
2020-07-19 07:44:10 -0400

    [New_Routes]

--- server.js
+++ server.js
@@ -847,6 +847,10 @@ const HN2_Rou=function( req , res ){ "use strict"
     ,   "/TAB_001_UPDATE"
         :[ "./SQL/TAB_001_UPDATE._" , "HN7_Run_fap" ]
 
+    ,   "/JOSH_VIEW":[ "./JOSH/IMG_VEW/H._" , "HT" ]
+    ,   "/JIV.HT"   :[ "./JOSH/IMG_VEW/H._" , "HT" ]
+    ,   "/JIV.JS"   :[ "./JOSH/IMG_VEW/J._" , "JS" ]
+
         //:M:Matching. Routes matching their served files.   ://
         //:-:This is so we can run files locally or on server://
         //:-:without changing the hard coded script files.   ://
